cee290c99abe3eff1b69a8c771663e3a9d57ae4b
felipe Rodrigues <feliperdf505@gmail.com>
2026-05-07 11:14:20 -0300

    atualização README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 1---termo
+## Material de aulas de python 
+###
--- python aula 2/python aula 2/atividades/PROJETO.py
+++ python aula 2/python aula 2/atividades/PROJETO.py
@@ -13,14 +13,13 @@ while True:
     horario = float(input("informe o horário de entrada."))
     tipo_acesso = int(input("qual é seu tipo de acesso? \n 1- ticket \n 2- tag \n"))
     if tipo_acesso == 2:
-        print("Acesso via TAG liberado. Seja bem vindo! - ", horario,)
+        print("Acesso via TAG liberado. Seja bem vindo! Seu horario de entrada foi", horario,)
     elif tipo_acesso == 1:
         if vagas_ocupadas < numero_vagas:
-            print("precione o botão a baixo.")
-        time.sleep(2)
+            input("precione o botão a baixo.")
         print("verificando vagas disponíveis...")
-        time.sleep(4)
-        print("vaga encontrada! pegue seu ticket e siga em frente. -", horario,)
+        time.sleep(2)
+        print("vaga encontrada! pegue seu ticket e siga em frente. Seu horario de entrada foi:", horario,)
         vagas_ocupadas += 1
         break
     else:
@@ -30,40 +29,34 @@ while True:
         vagas_ocupadas += 1
         print("vagas ocupadas:", vagas_ocupadas)
         break
-
-import time 
-
 valor = 0
 
 print("Saída do estacionamento.")
+print("saída sem ticket.")
+print("taxa: R$ 50,00")
+valor = 50
 
-ticket = input("você possui o ticket de entrada? \n 1-sim \n 2-não \n")
-if ticket == "2":
-    print("saída sem ticket.")
-    print("taxa: R$ 50,00")
-    valor = 50
-else:
-    tempo = float(input("quantas horas você utilizou o estacionamnto?"))
-    if tempo <= 0.25:
+tempo = float(input("quantas horas você utilizou o estacionamnto?"))
+if tempo <= 0.25:
         valor = 0
         print("tempo de toleância. Gratuito.")
-    elif tempo <= 3:
+elif tempo <= 3:
         valor = 15
         print("valor fixo de R$ 15.00.")
-    else:
+else:
         horas_extras = int(tempo -3)
         if tempo - 3 > horas_extras:
             horas_extras += 1
         print("Cobrança por horas extras aplicada.")
-    tag = input("você possui TAG? \n 1-sim \n 2-não \n")
-    if tag == "1":
+tag = input("você possui TAG? \n 1-sim \n 2-não \n")
+if tag == "1":
         valor = valor * 0.9
         print("Desconto de 10% para TAG aplicado.")
-    print("Valor total a pagar: R$", valor,".")
-    pago = input("Pagamento realizado? \n 1-sim \n 2-não \n")
-    if pago == "1":
+print("Valor total a pagar: R$", valor,".")
+pago = input("Pagamento realizado? \n 1-sim \n 2-não \n")
+if pago == "1":
         print("Pagamento confirmado. Cancela liberada.")
-    else:
+else:
         print("Pagamento pendente. Saída bloqueada.")
 
         
