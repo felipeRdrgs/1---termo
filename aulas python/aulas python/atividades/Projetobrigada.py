456f5ae3641598d5a883adc44728f812ce544a91
felipe rodrigues <feliperdf505@gmail.com>
2026-05-15 11:23:08 -0300

    projeto brigada

---
+++ aulas python/aulas python/atividades/Projetobrigada.py
@@ -0,0 +1,50 @@
+# 1. Cadastro de Funcionários:
+# ○ Armazene o nome, setor e o status dos treinamentos (NR-10, NR-35 e
+# Brigada).
+
+# 2. Verificação de EPI (NR-6):
+# ○ O sistema deve receber o setor do funcionário.
+# ○ Se o setor for "Elétrica", liste a obrigatoriedade de luvas de alta tensão e
+# botas dielétricas.
+# ○ Se o setor for "Trabalho em Altura", liste o cinturão de segurança e
+# talabarte.
+# 3. Alerta de Reciclagem:
+# ○ Crie uma função que receba o ano do último treinamento da Brigada de
+# Incêndio.
+# ○ Se o treinamento tiver mais de 2 anos, exiba a mensagem: "Treinamento
+# Vencido! Encaminhar para reciclagem."
+# ○ Caso contrário, exiba: "Treinamento Válido."
+
+
+# Requisitos Funcionais (RF):
+# .O sistema deve armazenar o nome, setor e status de treinamento de cada funcionario.
+# .Se o setor for da eletrica, listar a obrigatoriedade de equipamentos como botas dielétricas e luvas de alta tensão 
+# .Se o setor for de Trabalho em Altura, liste o cinturão de segurança e talabarte.
+# Exiba na tela um resumo com o total de funcionários cadastrados e quantos estão com treinamentos em dia.
+
+# Requisitos Funcionais (RF):
+# O sistema deve receber o setor do funcionário.
+# Crie uma função que receba o ano do último treinamento da Brigada de incêndio
+# Se o treinamento tiver mais de 2 anos, exiba a mensagem: "Treinamento Vencido! Encaminhar para reciclagem."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
