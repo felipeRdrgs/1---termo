5f5e836ac778a5f41636ba1396981208316b3500
felipe rodrigues <feliperdf505@gmail.com>
2026-05-22 11:22:18 -0300

    atividade projeto brigada

--- aulas python/aulas python/atividades/Projetobrigada.py
+++ aulas python/aulas python/atividades/Projetobrigada.py
@@ -27,16 +27,45 @@
 # Crie uma função que receba o ano do último treinamento da Brigada de incêndio
 # Se o treinamento tiver mais de 2 anos, exiba a mensagem: "Treinamento Vencido! Encaminhar para reciclagem."
 
+def identificação_de_funcionario():
+    print("Projeto Brigada: Sistema de Gestão de Segurança do Trabalho (SESMT)")
+    nome_funcionario = input("Qual é o seu nome? ")
+    setor_funcionario = input("Qual é o seu setor? \n1- elétrica \n2- trabalho em altura\n")
 
+    if setor_funcionario == "1":
+        print("Verificação de EPI.")
+        print("Itens obrigatórios a serem utilizados neste setor:\n1- luvas de alta tensão\n2- botas dielétricas")
+    elif setor_funcionario == "2":
+        print("Verificação de EPI.")
+        print("Itens obrigatórios a serem utilizados neste setor:\n1- cinturão de segurança\n2- talabarte")
+    else:
+        print("Setor não reconhecido. Informe 1 ou 2.")
 
+    resposta_epi = input("Você possui os itens listados acima? \n1- S \n2- N\n")
+    if resposta_epi == "1" or resposta_epi.upper() == "S":
+        print("Exigências cumpridas.")
+    elif resposta_epi == "2" or resposta_epi.upper() == "N":
+        print("Você não possui os itens obrigatórios. Certifique-se de possuí-los.")
+    else:
+        print("Resposta inválida.")
 
+    return nome_funcionario, setor_funcionario
 
+while True:
+    nome, setor = identificação_de_funcionario()
+    print(f"Bem-vindo funcionário. Você está sendo identificado como {nome}, setor {setor}.")
+    break
 
+ano_treinamento = float(input("digite o ano do último treinamento da brigada de incendio:"))
 
+if ano_treinamento > 2:
+    print("treinamento vencido! encaminhar para reciclagem.")
+else:
+    print("treinamento válido.")
 
-
-
-
+total_funcionarios = 1
+funcionarios_treinados = 1 
+print(f"Total de funcionários cadastrados: {total_funcionarios} \nFuncionários com treinamentos em dia: {funcionarios_treinados}")
 
 
 
